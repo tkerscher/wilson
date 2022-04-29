bc94e4faccb8b487962359ae5a037a898dc920e6
tkerscher <88444139+tkerscher@users.noreply.github.com>
2022-04-29 20:55:43 +0200

    Initial scaffold

---
+++ README.md
@@ -0,0 +1,7 @@
+# <img src="p-one.png" height=150>
+
+## About
+
+The P-ONE Observation Animator (P1ON) is a tool to animate and present neutrino events.
+
+Work in Progress...
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import WIPDisclaimer from './components/WIPDisclaimer.vue'
+</script>
+
+<template>
+  <WIPDisclaimer />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
