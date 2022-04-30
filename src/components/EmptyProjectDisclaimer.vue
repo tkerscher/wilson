8c6f48eb47ecc8189f9d031b6434b00529c806bd
tkerscher <88444139+tkerscher@users.noreply.github.com>
2022-04-30 21:52:35 +0200

    added open file button on empty disclaimer

--- src/components/EmptyProjectDisclaimer.vue
+++ src/components/EmptyProjectDisclaimer.vue
@@ -5,9 +5,25 @@
             Currently, there is nothing to show! <br />
             Select a file to watch it.
         </p>
-        
+        <label for="file-upload" class="p-button">
+            <CloudUploadIcon class="icon" />Open File
+        </label>
+        <input id="file-upload" type="file" accept=".p1on" @change="onFileSelected"/>
     </div>
 </template>
+
+<script setup lang="ts">
+import { CloudUploadIcon } from '@heroicons/vue/solid'
+import { useProject } from '../stores/project'
+const project = useProject()
+
+function onFileSelected(e: Event) {
+    const fileInput = e.target! as HTMLInputElement
+    if (fileInput.files) {
+        project.loadProject(fileInput.files[0])
+    }
+}
+</script>
 
 <style scoped>
 .container {
@@ -30,5 +46,9 @@
     font-size: x-large;
     font-weight: bolder;
     color: white;
+}
+
+input[type="file"] {
+    display: none;
 }
 </style>
