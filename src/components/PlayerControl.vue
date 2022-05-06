7cee34f208cb0e9fc730ea220d74407eca7f4963
tkerscher <88444139+tkerscher@users.noreply.github.com>
2022-05-07 00:20:53 +0200

    updated player control margins

--- src/components/PlayerControl.vue
+++ src/components/PlayerControl.vue
@@ -1,37 +1,39 @@
 <template>
-<div class="player-control-container">
-    <div class="top-action-bar">
-        <div class="left-action-group">
-            <div :class="['action-button', player.isCameraLocked ? 'camera-fixed-icon' : 'camera-icon']"
-                 role="button" title="Fix Camera" @mouseup="player.toggleCameraLocked"></div>
-            <div class="action-button info-icon" role="button" title="Show Project Info" @mouseup="emits('OpenInfoClicked')"></div>
+<div class="background-container">
+    <div class="player-control-container">
+        <div class="top-action-bar">
+            <div class="left-action-group">
+                <div :class="['action-button', player.isCameraLocked ? 'camera-fixed-icon' : 'camera-icon']"
+                     role="button" title="Fix Camera" @mouseup="player.toggleCameraLocked"></div>
+                <div class="action-button info-icon" role="button" title="Show Project Info" @mouseup="emits('OpenInfoClicked')"></div>
+            </div>
+            <div class="middle-action-group">
+                <div class="action-button rewind-icon" role="button" title="Rewind 10 sec" @mouseup="player.backward"></div>
+                <div :class="['action-button', 'play-button', player.isPlaying ? 'pause-icon' : 'play-icon']"
+                     role="button" title="Play/Pause" @mouseup="player.togglePlaying"></div>
+                <div class="action-button forward-icon" role="button" title="Forward 10 sec" @mouseup="player.forward"></div>
+                <div :class="['action-button', 'repeat-icon', player.isLooping ? 'toggle-active' : '']"
+                     role="button" title="Toggle Repeat" @mouseup="player.toggleLooping"></div>
+            </div>
+            <div class="right-action-group">
+                <DialInput :min-value="0.01"
+                           :max-value="1000"
+                           prefix="x "
+                           style="width: 80px"
+                           v-model="player.speedRatio" />
+                <div :class="['action-button', player.isFullscreen ? 'compress-icon' : 'expand-icon']"
+                     role="button" title="Toggle Fullscreen" @mouseup="player.toggleFullscreen"></div>
+            </div>
         </div>
-        <div class="middle-action-group">
-            <div class="action-button rewind-icon" role="button" title="Rewind 10 sec" @mouseup="player.backward"></div>
-            <div :class="['action-button', 'play-button', player.isPlaying ? 'pause-icon' : 'play-icon']"
-                 role="button" title="Play/Pause" @mouseup="player.togglePlaying"></div>
-            <div class="action-button forward-icon" role="button" title="Forward 10 sec" @mouseup="player.forward"></div>
-            <div :class="['action-button', 'repeat-icon', player.isLooping ? 'toggle-active' : '']"
-                 role="button" title="Toggle Repeat" @mouseup="player.toggleLooping"></div>
+        <div class="bottom-action-bar">
+            <span class="frame-counter">{{player.currentFrame.toFixed(2)}}</span>
+            <Slider :min-value="player.startFrame"
+                    :max-value="player.endFrame"
+                    v-model="player.currentFrame"
+                    @scrubbing-start="emits('scrubbingStart')"
+                    @scrubbing-end="emits('scrubbingEnd')" />
+            <span class="frame-counter">{{player.endFrame.toFixed(2)}}</span>
         </div>
-        <div class="right-action-group">
-            <DialInput :min-value="0.01"
-                       :max-value="1000"
-                       prefix="x "
-                       style="width: 80px"
-                       v-model="player.speedRatio" />
-            <div :class="['action-button', player.isFullscreen ? 'compress-icon' : 'expand-icon']"
-                 role="button" title="Toggle Fullscreen" @mouseup="player.toggleFullscreen"></div>
-        </div>
-    </div>
-    <div class="bottom-action-bar">
-        <span class="frame-counter">{{player.currentFrame.toFixed(2)}}</span>
-        <Slider :min-value="player.startFrame"
-                :max-value="player.endFrame"
-                v-model="player.currentFrame"
-                @scrubbing-start="emits('scrubbingStart')"
-                @scrubbing-end="emits('scrubbingEnd')" />
-        <span class="frame-counter">{{player.endFrame.toFixed(2)}}</span>
     </div>
 </div>
 </template>
@@ -50,35 +52,44 @@ const emits = defineEmits<{
 </script>
 
 <style scoped>
+.background-container {
+    width: 100%;
+    height: 90px;
+    background-color: #2c2c2c;
+}
 .player-control-container {
     width: 100%;
-    height: 80px;
+    max-width: 1500px;
+    margin: auto;
+    height: 100%;
     display: flex;
     flex-direction: column;
-    background-color: #2c2c2c;
 }
 
 .top-action-bar {
     width: 100%;
-    flex: 2;
+    flex: 1;
     display: flex;
+    box-sizing: border-box;
 }
 .top-action-bar > * {
     flex: 1;
     display: flex;
     align-items: center;
-    margin: 0 10px;
     column-gap: 16px;
+}
+.left-action-group {
+    margin-left: 25px;
 }
 .middle-action-group {
     justify-content: center;
-    padding-left: 30px;
 }
 .right-action-group {
     justify-content: right;
+    margin-right: 20px;
 }
 .bottom-action-bar {
-    flex: 1;
+    height: 20px;
     display: flex;
     color: white;
     margin: 10px;
