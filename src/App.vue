c0089f6ac454f929a8daaddd64a4d50410b25437
tkerscher <88444139+tkerscher@users.noreply.github.com>
2022-09-04 20:58:12 +0200

    added page loading spinner

--- src/App.vue
+++ src/App.vue
@@ -15,19 +15,3 @@ import ProjectView from './components/ProjectView.vue'
 import { useProject } from './stores/project'
 const project = useProject()
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
-}
-</style>
