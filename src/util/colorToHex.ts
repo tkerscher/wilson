330d9d542a21b6bd336bd277593690a9ab120585
tkerscher <88444139+tkerscher@users.noreply.github.com>
2022-09-07 00:33:56 +0200

    fixed wrong float to byte conversion

--- src/util/colorToHex.ts
+++ src/util/colorToHex.ts
@@ -1,4 +1,4 @@
 export function toHex(r: number, g: number, b: number, a: number) {
-    const hex = (x: number) => Math.floor(x).toString(16)
+    const hex = (x: number) => Math.round(x*255).toString(16)
     return hex(r) + hex(g) + hex(b) + hex(a)
 }
