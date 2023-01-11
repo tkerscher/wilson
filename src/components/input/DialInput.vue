665993989ac5ddec749b90d8eccb60cdf49572bb
tkerscher <88444139+tkerscher@users.noreply.github.com>
2023-01-11 20:54:15 +0100

    added colormap control in UI

--- src/components/input/DialInput.vue
+++ src/components/input/DialInput.vue
@@ -35,9 +35,9 @@ const fineSpeed = 0.01;
 
 const props = defineProps<{
     modelValue: number,
-    minValue: number,
-    maxValue: number,
-    prefix: string,
+    minValue?: number,
+    maxValue?: number,
+    prefix?: string,
 }>();
 const emits = defineEmits<{
     (e: "update:modelValue", value: number): void
@@ -48,9 +48,6 @@ const valid = ref(true);
 const editDiv = ref<HTMLDivElement|null>(null);
 
 function startEdit() {
-    if (!editDiv.value)
-        return;
-
     isEditing.value = true;
     valid.value = true;
     nextTick(() => {
@@ -95,10 +92,10 @@ function onWheel(e: WheelEvent) {
 
     const delta = (e.altKey ? fineSpeed : normalSpeed) * (e.deltaY < 0 ? 1 : -1);
     var newValue = props.modelValue + delta;
-    if (newValue < props.minValue) {
+    if (props.minValue && newValue < props.minValue) {
         newValue = props.minValue;
     }
-    else if (newValue > props.maxValue) {
+    else if (props.maxValue && newValue > props.maxValue) {
         newValue = props.maxValue;
     }
     emits("update:modelValue", newValue);
@@ -115,10 +112,10 @@ function endDrag() {
 function dragging(e: MouseEvent) {
     const delta = e.movementX * (e.altKey ? fineSpeed : normalSpeed);
     var newValue = props.modelValue + delta;
-    if (newValue < props.minValue) {
+    if (props.minValue && newValue < props.minValue) {
         newValue = props.minValue;
     }
-    else if (newValue > props.maxValue) {
+    else if (props.maxValue && newValue > props.maxValue) {
         newValue = props.maxValue;
     }
     emits("update:modelValue", newValue);
@@ -138,7 +135,7 @@ function dragging(e: MouseEvent) {
 }
 
 .input-edit {
-    background-color: white;
+    background-color: var(--primary1);
     height: 18px;
     border-radius: 50px;
     margin-bottom: -2px;
---
+++ src/components/tabs/ColorMapControl.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="cmap-root">
+    <canvas
+      ref="cmapCanvas"
+      class="cmap-canvas"
+      width="1"
+      height="256"
+    />
+    <div class="sidebar">
+      <DialInput
+        class="slider"
+        :model-value="minValue"
+        @update:model-value="updateMinValue"
+      />
+      <div class="spacer" />
+      <DialInput
+        class="slider"
+        :model-value="maxValue"
+        @update:model-value="updateMaxValue"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import DialInput from "../input/DialInput.vue";
+import { onMounted, ref } from "vue";
+
+import { renderColormap } from "../../util/color";
+import { useProject } from "../../stores/project";
+import { SceneCommander } from "../../scene/bus/commandBus";
+const project = useProject();
+
+const minValue = ref(1.0);
+function updateMinValue(value: number) {
+    if (value > maxValue.value)
+        value = maxValue.value;
+    minValue.value = value;
+    SceneCommander.SetColormapMinScalar(value);
+}
+const maxValue = ref(0.0);
+function updateMaxValue(value: number) {
+    if (value < minValue.value)
+        value = minValue.value;
+    maxValue.value = value;
+    SceneCommander.SetColormapMaxScalar(value);
+}
+
+const cmapCanvas = ref<HTMLCanvasElement|null>(null);
+async function updateCanvas() {
+    if (!cmapCanvas.value)
+        return;
+
+    //render color map
+    const size = 1024;
+    const dataArray = new Uint8ClampedArray(
+        renderColormap(project.colormap, size).buffer);
+    const imageData = new ImageData(dataArray, 1);
+    const bitmap = await createImageBitmap(imageData);
+
+    //update canvas
+    const canvas = cmapCanvas.value;
+    canvas.width = 1;
+    canvas.height = imageData.height;
+    //render colormap as bitmap
+    const ctx = canvas.getContext("bitmaprenderer");
+    if (!ctx)
+        return;
+    ctx.transferFromImageBitmap(bitmap);
+}
+project.$subscribe(updateCanvas);
+onMounted(() => {
+    const cmap = project.colormap;
+    if (!cmap || cmap.stops.length <= 1) {
+        //min max cant be equal, otherwise the shader crashes
+        minValue.value = 0.0;
+        maxValue.value = 1.0;
+    }
+    else {
+        minValue.value = cmap.stops[0].value;
+        maxValue.value = cmap.stops[cmap.stops.length - 1].value;
+    }
+    updateCanvas();
+});
+</script>
+
+<style scoped>
+.cmap-root {
+    margin: 10px;
+    display: flex;
+    flex-direction: row;
+}
+.sidebar {
+    display: flex;
+    flex-direction: column;
+    width: 100px;
+}
+
+.cmap-canvas {
+    width: 45px;
+    height: 500px;
+    grid-column: 1/3;
+    grid-row: 1;
+    border: 3px solid black;
+}
+
+.spacer {
+    flex: 1;
+}
+.slider {
+    margin: 5px;
+}
+</style>
