01b810971353d68a2c6e4fe0b03276320a050649
tkerscher <88444139+tkerscher@users.noreply.github.com>
2023-07-10 21:04:42 +0200

    pulled colormap rendering into own util function

--- src/scene/materials/colormapController.ts
+++ src/scene/materials/colormapController.ts
@@ -5,6 +5,7 @@ import { Texture } from "@babylonjs/core/Materials/Textures/texture";
 
 import { ColorMap } from "../../model/colormap";
 import { interpolateColormap } from "../../interpolation/colormap";
+import { renderColormap } from "../../util/color";
 
 const TEXTURE_SIZE = 1024;
 
@@ -32,7 +33,9 @@ export class ColorMapController {
             this.#sanitizeRange();
         }
 
-        this.#texture = createTexture(scene, colormap);
+        const data = renderColormap(colormap, TEXTURE_SIZE);
+        this.#texture = RawTexture.CreateRGBATexture(
+            data, TEXTURE_SIZE, 1, scene, false, false);
         this.#texture.coordinatesMode = Texture.EQUIRECTANGULAR_MODE;
     }
 
@@ -76,44 +79,5 @@ export class ColorMapController {
         if (this.#minScalar == this.#maxScalar) {
             this.#maxScalar += 1e-7; //float epsilon
         }
-    }
-}
-
-function createTexture(scene: Scene, colormap?: ColorMap): RawTexture {
-    if (!colormap || colormap.stops.length == 0) {
-        //create default colormap if none is provided (solid black)
-        const data = new Uint8Array([0, 0, 0, 255]);
-        return RawTexture.CreateRGBATexture(data, 1, 1, scene);
-    }
-    else if (colormap.stops.length == 1) {
-        //create single color texture
-        const color = colormap.stops[0].color;
-        const data = new Uint8Array([
-            (color?.r ?? 0) * 255,
-            (color?.g ?? 0) * 255,
-            (color?.b ?? 0) * 255,
-            (color?.a ?? 0) * 255
-        ]);
-        return RawTexture.CreateRGBATexture(data, 1, 1, scene);
-    }
-    else {
-        //build color map texture
-        const start = colormap.stops[0].value;
-        const end = colormap.stops[colormap.stops.length - 1].value;
-        const period = end - start;
-        const data = new Uint8Array(function*() {
-            //Interpolate to guarantee uniform steps in texture
-            for (let i = 0; i < TEXTURE_SIZE; ++i) {
-                const t = i / TEXTURE_SIZE * period + start;
-                const c = interpolateColormap(colormap, t);
-
-                yield c.r * 255;
-                yield c.g * 255;
-                yield c.b * 255;
-                yield c.a * 255;
-            }
-        }());
-        return RawTexture.CreateRGBATexture(data,
-            TEXTURE_SIZE, 1, scene, false, false);
     }
 }
